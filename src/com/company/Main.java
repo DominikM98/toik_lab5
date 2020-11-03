3fae9bd778af4858674375e3fdcee2b6f6490319
Dominik Majcher <majcher.dominik@op.pl>
2020-11-03 13:31:42 +0100

    implements classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("ds");
+        StackOperations stacks = new Stack();
+
+        stacks.push("Ogorki");
+        stacks.push("Pomidor");
+        stacks.push("Papier");
+        stacks.push("Czajnik");
+
+        System.out.println("Stos wygląda następująco: "+stacks.get());
+
+        System.out.println(stacks.pop());
+        System.out.println(stacks.pop());
+        System.out.println(stacks.pop());
+       // System.out.println(stacks.pop());
+
+        System.out.println("Stos po usunięciu kolejnych elementów: "+stacks.get());
     }
 }
--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -1,21 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class Stack implements StackOperations{
+
+    private final List<String> list = new ArrayList<>();
     @Override
     public List<String> get() {
-        return null;
+        return list;
     }
 
     @Override
     public Optional<String> pop() {
-        return Optional.empty();
+        if(list.isEmpty()) {
+            return Optional.empty();
+        }
+
+        String head_list = list.get(0);
+        list.remove(0);
+
+        return Optional.of(head_list);
     }
 
     @Override
     public void push(String item) {
-
+        list.add(0,item);
+        System.out.println("Do stosu dodano: "+item);
     }
 }
